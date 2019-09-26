2c8572f3c023243db692ec63f559237e9b61340a
Gordiig <gordiig@gmail.com>
2019-09-26 19:01:33 +0300

    Fixed urls and added migration

--- Messages/MessagesApp/urls.py
+++ Messages/MessagesApp/urls.py
@@ -3,7 +3,7 @@ from MessagesApp import views
 
 
 url_patterns = [
-    url('^messages/all/$', views.AllMessagesView.as_view()),
-    url('^messages/(?P<user_id>\d+)/$', views.MessagesView.as_view()),
-    url('^messages/(?P<message_id>\w+)/$', views.ConcreteMessageView.as_view()),
+    url(r'^messages/all/$', views.AllMessagesView.as_view()),
+    url(r'^messages/(?P<user_id>\d+)/$', views.MessagesView.as_view()),
+    url(r'^messages/(?P<message_id>\w+)/$', views.ConcreteMessageView.as_view()),
 ]
