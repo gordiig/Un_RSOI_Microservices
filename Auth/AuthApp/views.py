51b79fc0906d5e2ce027c17372f6c5f114e8d76e
Gordiig <gordiig@gmail.com>
2019-09-26 17:41:28 +0300

    Add view and urls

--- Auth/AuthApp/views.py
+++ Auth/AuthApp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import Request, Response, APIView
+from rest_framework.permissions import IsAuthenticated
+from AuthApp.serializers import UserSerializer
 
-# Create your views here.
+
+class UserInfoGetterView(APIView):
+    """
+    Вьюха для получения информации о юзере
+    """
+    permission_classes = (IsAuthenticated, )
+
+    def get(self, request: Request, *args, **kwargs):
+        if request.user is None:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        serializer = UserSerializer(instance=request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
