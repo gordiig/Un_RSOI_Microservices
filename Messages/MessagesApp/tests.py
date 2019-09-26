ad778651638ab6a7577011a1aee7ac3e48d3512d
Gordiig <gordiig@gmail.com>
2019-09-26 19:25:00 +0300

    Add AllTestCase

--- Messages/MessagesApp/tests.py
+++ Messages/MessagesApp/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from TestUtils.models import BaseTestCase
+from MessagesApp.models import Message
 
-# Create your tests here.
+
+class AllMessagesTestCase(BaseTestCase):
+    """
+    Тест для получения списка всех сообщений
+    """
+    def setUp(self):
+        super().setUp()
+        self.url = self.url_prefix + 'messages/all/'
+        self.message, _ = Message.objects.get_or_create(
+            from_user_id=1,
+            to_user_id=1,
+            text='Text'
+        )
+
+    def testGet(self):
+        response = self.get_response_and_check_status(url=self.url, expected_status_code=200)
+        self.assertEqual(len(response), 1)
+        msg = response[0]
+        self.assertEqual(msg['text'], self.message.text)
+        self.assertEqual(msg['uuid'], str(self.message.uuid))
