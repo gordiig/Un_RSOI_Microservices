6c6d353419b52a19ce4a94e5c5213ba5fd74a50b
Gordiig <gordiig@gmail.com>
2019-09-26 18:53:51 +0300

    Fixed serializer

--- Messages/MessagesApp/serializers.py
+++ Messages/MessagesApp/serializers.py
@@ -4,42 +4,19 @@ from MessagesApp.models import Message
 
 class MessageSerializer(serializers.ModelSerializer):
     """
-    Сериалиатор сообщения для списка (без подтягивания самого изображения и аудио)
+    Сериалиатор сообщения
     """
-    has_image = serializers.SerializerMethodField()
-    has_audio = serializers.SerializerMethodField()
-
     class Meta:
         model = Message
         fields = [
             'uuid',
+            'from_user_id',
+            'to_user_id',
             'text',
-            'has_image',
-            'has_audio',
+            'image_uuid',
+            'audio_uuid',
         ]
 
-    def get_has_image(self, instance: Message):
-        return instance.image_uuid is not None
-
-    def get_has_audio(self, instance: Message):
-        return instance.audio_uuid is not None
-
-
-class ConcreteMessageSerializer(MessageSerializer):
-    """
-    Сериализатор конкретного сообщения
-    """
-    image = serializers.SerializerMethodField()
-    audio = serializers.SerializerMethodField()
-
-    class Meta(MessageSerializer.Meta):
-        fields = MessageSerializer.Meta.fields + [
-            'image',
-            'audio',
-        ]
-
-    def get_image(self, instance: Message):
-        return self.context['image']
-
-    def get_audio(self, instance: Message):
-        return self.context['audio']
+    def create(self, validated_data):
+        new = Message.objects.create(**validated_data)
+        return new
