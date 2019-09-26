b84cffc31e8edf9940d07a10f6db221c76251f9e
Gordiig <gordiig@gmail.com>
2019-09-26 17:24:37 +0300

    Add auth-token for rest_framework

--- Auth/Auth/urls.py
+++ Auth/Auth/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
+from django.urls import path, include
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^api/token-auth/', views.obtain_auth_token),
+    url(r'^api/', include('AuthApp.urls')),
 ]
