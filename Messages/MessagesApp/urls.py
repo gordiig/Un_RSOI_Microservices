b1daff0358d7a650d88f6ef1ed6bd6bb0132c808
Gordiig <gordiig@gmail.com>
2019-09-26 19:54:57 +0300

    Fixed urls and views

--- Messages/MessagesApp/urls.py
+++ Messages/MessagesApp/urls.py
@@ -3,7 +3,6 @@ from MessagesApp import views
 
 
 urlpatterns = [
-    url(r'^messages/all/$', views.AllMessagesView.as_view()),
-    url(r'^messages/(?P<user_id>\d+)/$', views.MessagesView.as_view()),
+    url(r'^messages/all/', views.AllMessagesView.as_view()),
     url(r'^messages/(?P<message_id>\w+)/$', views.ConcreteMessageView.as_view()),
 ]
--- Messages/MessagesApp/views.py
+++ Messages/MessagesApp/views.py
@@ -6,26 +6,31 @@ from MessagesApp.serializers import MessageSerializer
 from MessagesApp.models import Message
 
 
-class AllMessagesView(ListCreateAPIView):
+class AllMessagesView(APIView):
     """
     Вьюха для вывода всех сообщений и добавления нового
     """
-    serializer_class = MessageSerializer
-
-    def get_queryset(self):
-        return Message.objects.all()
-
-
-class MessagesView(APIView):
-    """
-    Вьюха для вывода всех сообщений пользователя
-    """
-    def get(self, request: Request, user_id):
+    def get(self, request: Request):
+        if len(request.query_params) == 0:
+            # Вообще все
+            return Response(MessageSerializer(instance=Message.objects.all(), many=True).data)
+        # Для определенного юзера
+        try:
+            user_id = request.query_params['user_id']
+        except KeyError:
+            return Response({'error': 'Wrong query params'}, status=status.HTTP_400_BAD_REQUEST)
         msgs = Message.objects.filter(Q(from_user_id=user_id) | Q(to_user_id=user_id))
         if len(msgs) == 0:
             return Response(status=status.HTTP_404_NOT_FOUND)
         serializer = MessageSerializer(instance=msgs, many=True)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request: Request):
+        serializer = MessageSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ConcreteMessageView(APIView):
