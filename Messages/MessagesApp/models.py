348e057e95297e302cfa76d77673151d70804236
Gordiig <gordiig@gmail.com>
2019-09-26 13:06:13 +0300

    Add Message model

--- Messages/MessagesApp/models.py
+++ Messages/MessagesApp/models.py
@@ -5,4 +5,10 @@ class Messages(models.Model):
     """
     Модель сообщения
     """
-    pass
+    uuid = models.UUIDField(primary_key=True)
+    text = models.CharField(null=True, max_length=2048)
+    image_uuid = models.UUIDField(null=True)
+    audio_uuid = models.UUIDField(null=True)
+
+    def __str__(self):
+        return f'Message, uuid={self.uuid}'
