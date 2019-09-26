4e9de0fad54cc8089eabd384247b311de96f7153
Gordiig <gordiig@gmail.com>
2019-09-26 19:30:17 +0300

    Add MessageTestCase

--- Messages/MessagesApp/tests.py
+++ Messages/MessagesApp/tests.py
@@ -21,3 +21,30 @@ class AllMessagesTestCase(BaseTestCase):
         msg = response[0]
         self.assertEqual(msg['text'], self.message.text)
         self.assertEqual(msg['uuid'], str(self.message.uuid))
+
+
+class MessagesTestCase(BaseTestCase):
+    """
+    Тест ендпоинта api/messages/<user_id>/
+    """
+    def setUp(self):
+        super().setUp()
+        self.message1, _ = Message.objects.get_or_create(
+            from_user_id=1,
+            to_user_id=2,
+            text='Text'
+        )
+        self.message2, _ = Message.objects.get_or_create(
+            from_user_id=2,
+            to_user_id=3,
+            text='Text2'
+        )
+        self.url_404 = self.url_prefix + f'messages/0/'
+        self.url_200 = self.url_prefix + f'messages/{self.message1.to_user_id}/'
+
+    def testGet404(self):
+        _ = self.get_response_and_check_status(url=self.url_404, expected_status_code=404)
+
+    def testGet200(self):
+        response = self.get_response_and_check_status(url=self.url_200)
+        self.assertEqual(len(response), 2)
