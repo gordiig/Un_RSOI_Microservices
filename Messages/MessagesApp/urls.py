67e34a635509db3021bc97cb3e9120785d50523e
Gordiig <gordiig@gmail.com>
2019-09-26 18:48:19 +0300

    Fixed urls

--- Messages/MessagesApp/urls.py
+++ Messages/MessagesApp/urls.py
@@ -4,6 +4,6 @@ from MessagesApp import views
 
 url_patterns = [
     url('^messages/all/$', views.AllMessagesView.as_view()),
-    url('^messages/$', views.MessagesView.as_view()),
+    url('^messages/(?P<user_id>\d+)/$', views.MessagesView.as_view()),
     url('^messages/(?P<message_id>\w+)/$', views.ConcreteMessageView.as_view()),
 ]
