761e5bc100de4c0374165edf1b7a41688b2e71a6
Gordiig <gordiig@gmail.com>
2019-09-26 18:59:26 +0300

    Added views

--- Messages/MessagesApp/views.py
+++ Messages/MessagesApp/views.py
@@ -1,14 +1,14 @@
 from rest_framework import status
 from rest_framework.views import APIView, Response, Request
-from rest_framework.generics import ListCreateAPIView, ListAPIView
-from rest_framework.permissions import IsAuthenticated
-from MessagesApp.serializers import MessageSerializer, ConcreteMessageSerializer
+from rest_framework.generics import ListCreateAPIView
+from django.db.models import Q
+from MessagesApp.serializers import MessageSerializer
 from MessagesApp.models import Message
 
 
-class AllMessagesView(ListAPIView):
+class AllMessagesView(ListCreateAPIView):
     """
-    Вьюха для вывода всех сообщений (просто для наглядности, понятно что это плохо)
+    Вьюха для вывода всех сообщений и добавления нового
     """
     serializer_class = MessageSerializer
 
@@ -20,13 +20,30 @@ class MessagesView(APIView):
     """
     Вьюха для вывода всех сообщений пользователя
     """
-    permission_classes = (IsAuthenticated, )
-    serializer_class = MessageSerializer
+    def get(self, request: Request, user_id):
+        msgs = Message.objects.filter(Q(from_user_id=user_id) | Q(to_user_id=user_id))
+        if len(msgs) == 0:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = MessageSerializer(instance=msgs, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
-    def get_queryset(self, request: Request, *args, **kwargs):
-        query_params = request.query_params
+
+class ConcreteMessageView(APIView):
+    """
+    Вьюха для отображения конкретного сообщения
+    """
+    def get(self, request: Request, message_id):
         try:
+            msg = Message.objects.get(pk=message_id)
+        except Message.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = MessageSerializer(data=msg)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
-
-    def post(self, request: Request, *args, **kwargs):
-        pass
+    def delete(self, request: Request, message_id):
+        try:
+            msg = Message.objects.get(pk=message_id)
+        except Message.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        msg.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
