4c31b37a1e3f00a416a8244fae5b846f9fe28fa8
Gordiig <gordiig@gmail.com>
2019-09-26 13:15:22 +0300

    Added Message serializer

--- Messages/MessagesApp/serializers.py
+++ Messages/MessagesApp/serializers.py
@@ -1,10 +1,45 @@
 from rest_framework import serializers
-from MessagesApp.models import Messages
+from MessagesApp.models import Message
 
 
 class MessageSerializer(serializers.ModelSerializer):
     """
     Сериалиатор сообщения для списка (без подтягивания самого изображения и аудио)
     """
+    has_image = serializers.SerializerMethodField()
+    has_audio = serializers.SerializerMethodField()
+
     class Meta:
-        model =
+        model = Message
+        fields = [
+            'uuid',
+            'text',
+            'has_image',
+            'has_audio',
+        ]
+
+    def get_has_image(self, instance: Message):
+        return instance.image_uuid is not None
+
+    def get_has_audio(self, instance: Message):
+        return instance.audio_uuid is not None
+
+
+class ConcreteMessageSerializer(MessageSerializer):
+    """
+    Сериализатор конкретного сообщения
+    """
+    image = serializers.SerializerMethodField()
+    audio = serializers.SerializerMethodField()
+
+    class Meta(MessageSerializer.Meta):
+        fields = MessageSerializer.Meta.fields + [
+            'image',
+            'audio',
+        ]
+
+    def get_image(self, instance: Message):
+        return self.context['image']
+
+    def get_audio(self, instance: Message):
+        return self.context['audio']
