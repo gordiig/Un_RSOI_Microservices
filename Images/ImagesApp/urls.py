90a48688f1b47cab74dc951147a4779d452e7d0f
Gordiig <gordiig@gmail.com>
2019-09-27 20:41:41 +0300

    Fixed urls and views

--- Images/ImagesApp/urls.py
+++ Images/ImagesApp/urls.py
@@ -3,7 +3,7 @@ from ImagesApp import views
 
 
 urlpatterns = [
-    url(r'^images/$', views.ImagesView),
+    url(r'^images/$', views.ImagesView.as_view()),
     url(r'^images/(?P<image_uuid>[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})/$',
-        views.ConcreteImageView),
+        views.ConcreteImageView.as_view()),
 ]
