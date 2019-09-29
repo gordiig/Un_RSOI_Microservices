2538e58e16679bcb39feb0c683aba438d0cea962
Gordiig <gordiig@gmail.com>
2019-09-29 15:56:12 +0300

    Add urls for new views

--- Gateway/GatewayApp/urls.py
+++ Gateway/GatewayApp/urls.py
@@ -3,6 +3,10 @@ from GatewayApp import views
 
 
 urlpatterns = [
+    url(r'^token-auth/$', views.AuthView.as_view()),
+    url(r'^register/$', views.RegisterView.as_view()),
+    url(r'^user_info/$', views.GetUserInfoView.as_view()),
+
     url(r'^audio/$', views.AudiosView.as_view()),
     url(r'^audio/(?P<audio_uuid>[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})/$',
         views.ConcreteAudioView.as_view()),
