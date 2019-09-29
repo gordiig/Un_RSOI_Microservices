d1a541d98081beb8272046702ace379cef24213e
Gordiig <gordiig@gmail.com>
2019-09-29 15:54:53 +0300

    Add some auth views

--- Gateway/GatewayApp/views.py
+++ Gateway/GatewayApp/views.py
@@ -3,6 +3,45 @@ from GatewayApp.requesters import Requester
 from GatewayApp.permissions import IsAuthenticatedThroughAuthService
 
 
+# MARK: - Auth
+class AuthView(APIView):
+    """
+    Получение токена по юзернейму и паролю
+    """
+    def post(self, request: Request):
+        if {'username', 'password'}.intersection(request.data.keys()) != 2:
+            return Response({'error': 'Body must have username and password fields'}, status=400)
+        response_json, code = Requester.authenticate(username=request.data['username'],
+                                                     password=request.data['password'])
+        return Response(response_json, status=code)
+
+
+class RegisterView(APIView):
+    """
+    Регистрация
+    """
+    def post(self, request: Request):
+        needed_data = {'username', 'password', 'email'}.intersection(request.data.keys())
+        if len(needed_data) != 3:
+            return Response({'error': 'Body must have username, email and password fields'}, status=400)
+        response_json, code = Requester.register(username=request.data['username'], password=request.data['password'],
+                                                 email=request.data['email'])
+        return Response(data=response_json, status=code)
+
+
+class GetUserInfoView(APIView):
+    """
+    Получение инфы о юзере по токену
+    """
+    def get(self, request: Request):
+        token_str = request.META.get('Authorization')
+        if token_str is None:
+            return Response({'error': 'No Authorization header!'}, status=400)
+        token = token_str[6:]
+        response_json, code = Requester.get_user_info(token)
+        return Response(response_json, status=code)
+
+
 # MARK: - Аудио
 class AudiosView(APIView):
     """
