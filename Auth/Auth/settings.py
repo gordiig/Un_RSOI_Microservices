d70506570b6ae6e824b610271120c3d77efc7aa2
Gordiig <gordiig@gmail.com>
2019-09-30 15:02:24 +0300

    Fixing auth

--- Auth/Auth/settings.py
+++ Auth/Auth/settings.py
@@ -82,7 +82,8 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': renderer_classes,
     'DEFAULT_AUTHENTICATION_CLASSES': (
        'rest_framework.authentication.TokenAuthentication',
-   ),
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
 }
 
 # Database
