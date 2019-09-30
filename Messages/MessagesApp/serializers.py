94e0e0234b8956c2704def891ce3be91e4333b92
Gordiig <gordiig@gmail.com>
2019-09-30 15:08:54 +0300

    Add update to serializer

--- Messages/MessagesApp/serializers.py
+++ Messages/MessagesApp/serializers.py
@@ -20,3 +20,10 @@ class MessageSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         new = Message.objects.create(**validated_data)
         return new
+
+    def update(self, instance: Message, validated_data):
+        instance.from_user_id = validated_data.get('from_user_id', instance.from_user_id)
+        instance.to_user_id = validated_data.get('to_user_id', instance.to_user_id)
+        instance.text = validated_data.get('text', instance.text)
+        instance.image_uuid = validated_data.get('image_uuid', instance.image_uuid)
+        instance.audio_uuid = validated_data.get('audio_uuid', instance.audio_uuid)
