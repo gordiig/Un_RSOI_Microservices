646a14052eea8f3a047c3eef00480d0cc0f3d209
Gordiig <gordiig@gmail.com>
2019-10-01 13:44:56 +0300

    Fided permissions

--- Gateway/GatewayApp/permissions.py
+++ Gateway/GatewayApp/permissions.py
@@ -1,5 +1,5 @@
 from rest_framework.permissions import BasePermission
-from GatewayApp.requesters import Requester
+from GatewayApp.requesters.auth_requester import AuthRequester
 
 
 class IsAuthenticatedThroughAuthService(BasePermission):
@@ -7,9 +7,5 @@ class IsAuthenticatedThroughAuthService(BasePermission):
     Пермишн класс для проверки токена на стороне Auth-сервиса
     """
     def has_permission(self, request, view):
-        token_str = request.META.get('HTTP_AUTHORIZATION')
-        if not token_str:
-            return False
-        token = token_str[6:]
-        _, code = Requester.get_user_info(token)
+        _, code = AuthRequester().get_user_info(request=request)
         return code == 200
