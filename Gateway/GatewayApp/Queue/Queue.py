ca696db8fb900ee8db7c299939d5f7cd4f8918b6
Gordiig <gordiig@gmail.com>
2019-11-25 17:31:16 +0300

    Now ok if code < 500

--- Gateway/GatewayApp/Queue/Queue.py
+++ Gateway/GatewayApp/Queue/Queue.py
@@ -28,21 +28,17 @@ class Queue:
     def fire_audio_tasks():
         atasks = list(filter(lambda x: x['type'] == Queue.AUDIO, Queue.queue))
         ar = AudioRequester()
-        ok_code = 000
         tasks_to_delete = []
         for i, task in atasks:
             if task['request'].method == 'POST':
                 _, code = ar.post_audio(task['request'], task['data'])
-                ok_code = 201
             elif task['request'].method == 'PATCH':
                 _, code = ar.patch_audio(task['request'], task['uuid'], task['data'])
-                ok_code = 202
             elif task['request'].method == 'DELETE':
                 _, code = ar.delete_audio(task['request'], task['uuid'])
-                ok_code = 204
             else:
-                code = -1
-            if code == ok_code:
+                code = 501
+            if code < 500:
                 tasks_to_delete.append(task)
         for task in tasks_to_delete:
             Queue.queue.remove(task)
@@ -51,21 +47,17 @@ class Queue:
     def fire_image_tasks():
         itasks = list(filter(lambda x: x['type'] == Queue.IMAGE, Queue.queue))
         ir = ImagesRequester()
-        ok_code = 000
         tasks_to_delete = []
         for task in itasks:
             if task['request'].method == 'POST':
                 _, code = ir.post_image(task['request'], task['data'])
-                ok_code = 201
             elif task['request'].method == 'PATCH':
                 _, code = ir.patch_image(task['request'], task['uuid'], task['data'])
-                ok_code = 202
             elif task['request'].method == 'DELETE':
                 _, code = ir.delete_image(task['request'], task['uuid'])
-                ok_code = 204
             else:
-                code = -1
-            if code == ok_code:
+                code = 501
+            if code < 500:
                 tasks_to_delete.append(task)
         for task in tasks_to_delete:
             Queue.queue.remove(task)
