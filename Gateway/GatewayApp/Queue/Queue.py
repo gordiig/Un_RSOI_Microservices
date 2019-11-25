8ae2968dea0da688e58efa4996e711c159515917
Gordiig <gordiig@gmail.com>
2019-11-25 17:34:33 +0300

    Add print logging

--- Gateway/GatewayApp/Queue/Queue.py
+++ Gateway/GatewayApp/Queue/Queue.py
@@ -19,13 +19,16 @@ class Queue:
     @staticmethod
     def add_image_task(request, data: Union[None, dict] = None, uuid: Union[None, str] = None):
         Queue._add_task_to_queue(request, data, uuid, ttype=Queue.IMAGE)
+        print('Add image task to queue')
 
     @staticmethod
     def add_audio_task(request, data: Union[None, dict] = None, uuid: Union[None, str] = None):
         Queue._add_task_to_queue(request, data, uuid, ttype=Queue.AUDIO)
+        print('Add audio task to queue')
 
     @staticmethod
     def fire_audio_tasks():
+        print('Firing audio tasks...')
         atasks = list(filter(lambda x: x['type'] == Queue.AUDIO, Queue.queue))
         ar = AudioRequester()
         tasks_to_delete = []
@@ -42,9 +45,11 @@ class Queue:
                 tasks_to_delete.append(task)
         for task in tasks_to_delete:
             Queue.queue.remove(task)
+        print(f'{len(tasks_to_delete)} audio tasks fired with success')
 
     @staticmethod
     def fire_image_tasks():
+        print('Firing image tasks...')
         itasks = list(filter(lambda x: x['type'] == Queue.IMAGE, Queue.queue))
         ir = ImagesRequester()
         tasks_to_delete = []
@@ -61,3 +66,4 @@ class Queue:
                 tasks_to_delete.append(task)
         for task in tasks_to_delete:
             Queue.queue.remove(task)
+        print(f'{len(tasks_to_delete)} image tasks fired with success')
